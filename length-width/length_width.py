1c162db12b010ae0d007a8003e95b2fc6a307de0
Danil <danililukhin@yandex.ru>
2023-07-09 23:10:18 +0500

    First homework added

---
+++ length-width/length_width.py
@@ -0,0 +1,10 @@
+length_square = int(input('Введите длину стороны квадрата числом: '))
+
+print('Периметр квадрата:', length_square * 4)
+print('Площадь квадрата:', length_square * 2)
+
+length_rectangle = int(input('Введите длину прямоугольника числом: '))
+width_rectangle = int(input('Введите ширину прямоугольника числом: '))
+
+print('Периметр прямоугольника:', length_rectangle * 2 + width_rectangle * 2)
+print('Площадь прямоугольника:', length_rectangle * width_rectangle)
